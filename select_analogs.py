1a0f597827e4f242ebac97f5eceaaeb5884213a9
Tori Bonidie <40264578+toribonidie@users.noreply.github.com>
2022-08-17 11:26:12 -0400

    Update select_analogs.py

--- select_analogs.py
+++ select_analogs.py
@@ -1,8 +1,28 @@
 from scipy import spatial
 import numpy as np
 import pandas as pd
+# Specifically written scripts for this work
+from analog_auto import analog_auto
 
 def select_analogs(training_sample, test_sample, test_stats, n_analogs, n_star, params, **kwargs):
+     """Selects a single Milky Way analog from the given catalogs.
+ 
+    Given a set of Milky Way parameters (e.g. Teff, log(g),[Fe/H])
+    will select a set of analog stars from a binary tree constructed
+    from the parameters using the analog_auto() function.
+    
+    
+    Args:
+        training_sample (dataframe): A dataframe containing the parameters where analogs are
+            being searched for. 
+        test_sample (dataframe): A dataframe containing the parameters of interest
+        n_analogs (int): The number of analogs to obtain.
+        params (array): An array of the parameters from which to select analogs.
+    Returns:
+        An array of indices of a sample of analog stars in the 
+        parameter space.
+    """
+    
     search_space = np.zeros((training_sample[params].shape[0], training_sample[params].shape[1]))
     for j, i in enumerate(params):
         search_space[:, j] = (training_sample[params[j]].values - test_stats.at["mean", params[j]]) / test_stats.at["sigma", params[j]]
